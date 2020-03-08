73541e7021b6f24c9a1558e6bc1c8b51d60ce1cc
Steven Lee <Steven.Lee@edu-kw1c.nl>
2020-03-08 22:52:57 +0100

    Opdracht 4.1

---
+++ Hoofdstuk 4/opdracht4.2.php
@@ -0,0 +1,69 @@
+<?php
+/**
+ * User: Steven Lee
+ * Date: 1-03-2020
+ * Time: 14:20 PM
+ * File: opdracht3.2.php
+ */
+?>
+
+<?php
+include "../Inlcude/header.php"
+?>
+
+<?php
+$courseName = "Engels";
+$teacherName = "";
+
+switch ($courseName)
+    // dit zijn de lerearen en de vakken die zei geven.
+{
+    case    "Nederlands":
+        $teacherName = "Meneer van de Ende";
+        break;
+
+    case    "PHP":
+        $teacherName = "Meneer Saebu";
+        break;
+
+    case    "Engels":
+        $teacherName = "Meneer Giessen";
+        break;
+
+    case    "Javascript":
+        $teacherName = "Meneer Evers";
+        break;
+
+    case    "ASP":
+        $teacherName = "Meneer van Meer";
+        break;
+
+    case    "SQL":
+        $teacherName = "Meneer van de Wetering";
+        break;
+
+    case    "ComputerTekenen":
+        $teacherName = "Meneer van de Berg";
+        break;
+
+    case    "Burgerschap":
+        $teacherName = "Mevrouw de Ruiter";
+        break;
+
+    case    "Modelleren":
+        $teacherName = "Meneer van Bijnen";
+        break;
+
+    case    "DigitaleVaardigheden":
+        $teacherName = "Mevrouw Pielage";
+        break;
+
+}
+
+echo '<p>Voor het vak <b>'.$courseName.'</b> heb je <b>'.$teacherName.'</b> als docent.'
+?>
+<br>
+<br>
+<?php
+include "../Inlcude/Footer.php"
+?>
