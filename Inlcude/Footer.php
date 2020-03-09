43814f442860b66c26326b0ff879dbf1d4a635a6
Steven Lee <Steven.Lee@edu-kw1c.nl>
2020-03-09 11:08:07 +0100

    var en footer

--- Inlcude/Footer.php
+++ Inlcude/Footer.php
@@ -1,2 +1,10 @@
 <button><a href="../index.php">Home</a></button>
+
+// echo van data naar gebruiker
+<?php
+include("Inlcude/Variabelen.php");
+echo("<div class='footer'>");
+    echo("&copy; $year ");
+    echo("$name</div>");
+?>
 </body>
