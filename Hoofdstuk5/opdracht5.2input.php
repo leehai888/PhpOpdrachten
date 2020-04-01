4949a57f185d3806a3ed154728261d53c3681cbc
Steven Lee <Steven.Lee@edu-kw1c.nl>
2020-04-01 21:08:52 +0200

    opdracht 5.2

---
+++ Hoofdstuk5/opdracht5.2input.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * User: Steven Lee
+ * Date: 31-03-2020
+ * Time: 16:20 PM
+ * File: opdracht5.2.php
+ */
+?>
+
+<?php
+include "../Inlcude/header.php"
+?>
+
+    <h1>Uitschrijfformulier KW1C</h1>
+    <hr>
+    <form action="Hoofdstuk5/opdracht5.2.php" method="get">
+        <label>Voor en achternaam</label>
+        <input name="Voor-Achternaam" type="text"><br>
+        <br>
+        <label>Studentennummer</label>
+        <input name="Studentennummer" type=number><br>
+        <br>
+        <label>Datum van uitschrijving</label>
+        <input name="DatumUitschrijving" type="text"><br>
+        <br>
+        <label>Reden van uitschrijving</label>
+        <select name="Reden">
+            <option value="Geen leuke opleiding">niet wat ik dacht</option>
+            <option value="Te moeilijk">Te moeilijk</option>
+            <option value="Niks van berijpen">ik begrijp het niet</option>
+            <option value="Switchen">Niks voor mij</option>
+        </select><br>
+        <br>
+        <label>Leerjaar</label><br>
+        <br>
+        <input name="Leerjaar" type="radio" value="1e leerjaar">1e leerjaar</input><br>
+        <input name="Leerjaar" type="radio" value="2e leerjaar">2e leerjaar</input><br>
+        <input name="Leerjaar" type="radio" value="3e leerjaar">3e leerjaar</input><br>
+        <input name="Aanmelden" type="checkbox" value="ja">Ik wil me aanmeldden bij de succesklas<br>
+        <input name="Verwijder" type="checkbox" value="ja">verwijder gegevens van mij uit het systeem<br>
+        <br>
+        <label>reden van uitschrijving</label><br>
+        <input name="Bericht" type="text"><br>
+
+        <input type="submit" name="verzenden" value="verzenden">
+    </form>
+
+    <br>
+
+<?php
+include "../Inlcude/Footer.php"
+?>
