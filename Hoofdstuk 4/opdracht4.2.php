385c3c22ac480f246af03a480b719ec27f6c958d
Steven Lee <Steven.Lee@edu-kw1c.nl>
2020-05-18 11:36:35 +0200

    4.2 klopt maar moest het moest in een apart script dus heb die copy gepaste en geincluded verder 4.1 staat in mijn footer die heb ik al gelinked dus vraag me af wrm ik geen punt voor kreeg

--- Hoofdstuk 4/opdracht4.2.php
+++ Hoofdstuk 4/opdracht4.2.php
@@ -12,55 +12,7 @@ include "../Inlcude/header.php"
 ?>
 
 <?php
-$courseName = "Engels";
-$teacherName = "";
-
-switch ($courseName)
-    // dit zijn de lerearen en de vakken die zei geven.
-{
-    case    "Nederlands":
-        $teacherName = "Meneer van de Ende";
-        break;
-
-    case    "PHP":
-        $teacherName = "Meneer Saebu";
-        break;
-
-    case    "Engels":
-        $teacherName = "Meneer Giessen";
-        break;
-
-    case    "Javascript":
-        $teacherName = "Meneer Evers";
-        break;
-
-    case    "ASP":
-        $teacherName = "Meneer van Meer";
-        break;
-
-    case    "SQL":
-        $teacherName = "Meneer van de Wetering";
-        break;
-
-    case    "ComputerTekenen":
-        $teacherName = "Meneer van de Berg";
-        break;
-
-    case    "Burgerschap":
-        $teacherName = "Mevrouw de Ruiter";
-        break;
-
-    case    "Modelleren":
-        $teacherName = "Meneer van Bijnen";
-        break;
-
-    case    "DigitaleVaardigheden":
-        $teacherName = "Mevrouw Pielage";
-        break;
-
-}
-
-echo '<p>Voor het vak <b>'.$courseName.'</b> heb je <b>'.$teacherName.'</b> als docent.'
+include "../Inlcude/script 4.2.php"
 ?>
 <br>
 <br>
